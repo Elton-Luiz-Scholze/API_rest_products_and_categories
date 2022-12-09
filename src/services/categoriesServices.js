eeb2f9f2bec23fb68eabc743d4724e4fedb7369a
Elton Luiz Scholze <elscholze@gmail.com>
2022-12-08 22:23:48 -0300

    feat: create tables and tested database connection

---
+++ src/services/categoriesServices.js
@@ -0,0 +1,15 @@
+import database from "../database"
+
+const listCategoriesService = async () => {
+    const queryResponse = await database.query(
+        `SELECT
+            *
+        FROM
+            categories
+        ;`
+    );
+
+    return [200, queryResponse.rows];
+}
+
+export { listCategoriesService };
